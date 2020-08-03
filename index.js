2d7a21c9737f1099955d35bf95a3543ab8693492
Clay Palumbo <claypalumbo@Clays-MacBook-Pro.local>
2020-08-03 17:33:31 -0400

    init push

---
+++ index.js
@@ -0,0 +1,51 @@
+import { of, fromEvent, timer } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { takeUntil, mergeMapTo, tap, finalize, switchMap, switchMapTo, filter, exhaustMap, map } from 'rxjs/operators';
+
+/*
+ * Any code samples you want to play with can go in this file.
+ * Updates will trigger a live reload on http://localhost:1234/
+ * after running npm start.
+ */
+
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const pollingStatus = document.getElementById("polling-status");
+const dogImage = document.getElementById("dog");
+const scrollBar = document.getElementById("scroll-bar");
+const bg = document.getElementById('bg');
+
+const calculateScrollPercent = (element) => {
+    const {scrollTop, scrollHeight, clientHeight} = element;
+    return (scrollTop / ( scrollHeight - clientHeight)) * 100;
+}
+
+//streams
+const startClick$ = fromEvent(
+    startButton, 'click'
+);
+
+const stopClick$ = fromEvent(
+    stopButton, 'click'
+);
+
+const scroll$ = fromEvent(document, 'scroll');
+const progress$ = scroll$.pipe(
+    map(({target}) => calculateScrollPercent(target.scrollingElement)),
+    tap((percent) => {
+        scrollBar.style.width = `${percent}%`
+        bg.style.backgroundPositionY = `${percent / 5}%`
+    })
+).subscribe(console.log);
+
+
+startClick$.pipe(
+    exhaustMap(() => timer(0, 5000).pipe(
+        tap(() => pollingStatus.innerHTML = "Start"),
+        switchMapTo(
+            ajax.getJSON("https://random.dog/woof.json")
+        ),
+        takeUntil(stopClick$),
+        finalize(() =>pollingStatus.innerHTML = "Stopped")
+    ))
+).subscribe(dog => dogImage.src = dog.url);
